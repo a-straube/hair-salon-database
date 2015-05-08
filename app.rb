2fdecd30a3a3a13eaca0339c8b3830170ed7c3c4
Axel <kurtastraube@gmail.com>
2015-05-08 11:06:34 -0700

    Add stylist_form and corresponding routes.

--- app.rb
+++ app.rb
@@ -11,3 +11,15 @@ get('/') do
   @stylists = Stylist.all()
   erb(:index)
 end
+
+get('/stylist_form') do
+  erb(:stylist_form)
+end
+
+post('/') do
+  name = params.fetch('name')
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:index)
+end
