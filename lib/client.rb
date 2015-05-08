c5a154c5aa42f41f2afb93ecf45f4888b301cd65
Axel <kurtastraube@gmail.com>
2015-05-08 10:09:45 -0700

    Added ==, all, and save methods for Client class, pass Client.all spec.

--- lib/client.rb
+++ lib/client.rb
@@ -5,5 +5,24 @@ class Client
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
   end
-  
+
+  define_method(:==) do |client2|
+    self.name().==(client2.name()).&(self.id().==(client2.id()))
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      name = client.fetch("name")
+      id = client.fetch("id").to_i()
+      clients.push(Client.new({:name => name, :id => id}))
+    end
+    clients
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -9,4 +9,13 @@ describe(Client) do
     end
   end
 
+  describe('.all') do
+    it("returns all of the clients in the database") do
+      client1 = Client.new({:name => "Valeri Jones", :id => nil})
+      client1.save()
+      client2 = Client.new({:name => "Abigail Lastname", :id => nil})
+      client2.save()
+      expect(Client.all()).to(eq([client1, client2]))
+    end
+  end
 end
