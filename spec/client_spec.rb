f43eadbd8ce70f6d8b8d354ae1b692e3b396c3f7
Axel <kurtastraube@gmail.com>
2015-05-08 10:04:13 -0700

    Add Client class, pass Client#name spec.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe(Client) do
+
+  describe('#name') do
+    it("returns the name of a client") do
+      client = Client.new({:name => "Lisa Smith", :id => nil})
+      expect(client.name()).to(eq("Lisa Smith"))
+    end
+  end
+
+end
